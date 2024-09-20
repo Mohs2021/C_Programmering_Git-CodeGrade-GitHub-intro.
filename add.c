051d15e09b9d71e4010a859bb1836ea58e0cd0e8
Mohs2021 <90088149+Mohs2021@users.noreply.github.com>
2024-09-20 15:22:49 +0200

    add_1

--- add.c
+++ add.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-int main(int argc, char *argv[]){
-    int sum = 0;
-    sum = atoi(argv[1]) + atoi(argv[2]);
+int is_number(const char *s) {
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (!isdigit(s[i]) && s[i] != '-') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("ERROR; wrong number of arguments\n");
+        return 1;
+    }
+
+    if (!is_number(argv[1]) || !is_number(argv[2])) {
+        printf("ERROR; invalid input\n");
+        return 1;
+    }
+
+    int sum = atoi(argv[1]) + atoi(argv[2]);
     printf("%d\n", sum);
+
     return 0;
 }
